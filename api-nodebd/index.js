45cd0b2780e7eb8d5070d81b27a6a2854469aa2b
Maiara Sousa Gomes <51339578+maiaragomes20@users.noreply.github.com>
2021-11-12 20:11:47 -0300

    Update index.js

--- api-nodebd/index.js
+++ api-nodebd/index.js
@@ -7,12 +7,27 @@ Tabelas.init(conexao)
 const app = customExpress()
 
 app.get('/usuario', conexao.getUsuario)
-app.get('/usuario/id', conexao.getUsuarioById)
+app.get('/pr/:id/:id_one', conexao.getEstadoPR)
+app.get('/sc/:id/:id_one', conexao.getEstadoSC)
+app.get('/rs/:id/:id_one', conexao.getEstadoRS)
+app.get('/usuario/:id', conexao.getUsuarioById)
 app.post('/usuario', conexao.createUsuario)
 app.put('/usuario/:id', conexao.updateUsuario)
 app.delete('/usuario/:id', conexao.deleteUsuario)
 
+app.get('/', function(req,res){
+        res.send('Oi');
+});
+
+
 app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+            
+
+
+
+
+
+
             
 
 
